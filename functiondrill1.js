79906d6b53b05d5706ac302e3d1df093728f1feb
Vendy Prum <vendy@thinkful.com>
2020-02-10 14:02:42 -0800

    new year

--- functiondrill1.js
+++ functiondrill1.js
@@ -1,5 +1,6 @@
-function createGreeting(){
-    return "Hello, my name is Bill and I am 50"; 
+function createGreeting(name, age){
+    const yearOfBirth = 2020 - age;
+    return `Hello, my name is Bill and I am 50 and was born in the ${yearOfBirth}.`; 
 }
 
 const greeting1 = createGreeting();
