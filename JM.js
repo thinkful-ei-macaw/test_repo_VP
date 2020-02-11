bd1cdf58ac5297630eeaeb3b77978e7c78ef3a84
Penelope Owens <owenspennie9@live.com>
2020-02-10 19:46:17 -0600

    JM.js

--- JM.js
+++ JM.js
@@ -2,8 +2,12 @@ function createGreeting( name, age) {
     const yob= getyearOfbirth(age)
 return `Hello, my name is ${name} My year of birth is ${yob}` ;
 }
-const myvalue=createGreeting ("Penelope", 15)
+const myvalue=createGreeting ("Penelope", -15)
 console.log(myvalue)
 function getyearOfbirth ( age){
+    if (age< 0){
+        throw new Error("age cannot be negative")
+        
+    }
     return 2020 - age
 }
