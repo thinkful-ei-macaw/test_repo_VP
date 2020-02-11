97c75d63a7e4caee27ba823ed1579fdbf954157f
Penelope Owens <owenspennie9@live.com>
2020-02-11 08:46:21 -0600

    MissyElliot.js

--- MissyElliot.js
+++ MissyElliot.js
@@ -2,17 +2,16 @@ function jediName('Missy,Elliot'){
     return 'MiEl'
 } 
 function beyond {
-   if  num (either positive or negative) it should be log out
-  'And Beyond'.  
-   if  num (is infinite and positive it should log out)'To infinity'
-   if  num ( is finite and negative it should log out) 'To Negative infinity'.
-   if num (is zero it should log out) 'Staying home'
+   if  num ('either positive or negative')`it should be logged out And Beyond.`;  
+   if  num ('is infinite and positive it should log out')`To infinity.`;
+   if  num ('is finite and negative it should log out') `To Negative infinity.`;
+   if  num ('is zero it should log out') `Staying home`;
 }
  function decode{
-      'craft block argon meter bells brown croon droop'
+      `craft block argon meter bells brown croon droop`;
 }
-function month('dates' 1-31);
-function LeapYear('February' ,1-28'or'29);
-  const randomNo = Math.floor(Math.random()*3)+1;
+function month(:'dates' 1-31);
+function LeapYear(:'February',1-28'or'29);
+  const randomNo = Math.floor(Math.random(28,29)*3)+1;
    
 }
