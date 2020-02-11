c4e21979aa703a6d7cf94bb001c3b8964b09e7bf
Penelope Owens <owenspennie9@live.com>
2020-02-11 11:25:50 -0600

    MissyElliot.js

--- MissyElliot.js
+++ MissyElliot.js
@@ -2,8 +2,8 @@ function jediName(Missy,Elliot){
     return 'MiEl'
 } 
 function beyond (num){
-if (!isFinite(num)){console.log ('And Beyond');
-if (num>0)`To infinity.`;
-if  (num<0) `To Negative infinity.`;
-if  (num ===0) `Staying home`;}
+if (!isFinite(num)){console.log ('And Beyond');}
+if (num>0) {console.log`To infinity.`;}
+if (num<0) {console.log `To Negative infinity.`;}
+if (num ===0) {console.log`Staying home`;}
 }
