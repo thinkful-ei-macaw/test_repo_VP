9814a82bb0c38b7f3007fe30a4314b2637a93a9e
Penelope Owens <owenspennie9@live.com>
2020-02-11 08:16:45 -0600

    MissyElliot.js

--- MissyElliot.js
+++ MissyElliot.js
@@ -2,10 +2,11 @@ function jediName('Missy,Elliot'){
     return 'MiEl'
 }
 function beyond {
-    if num( either positive or negative) it should be log out
-  'And Beyond'.  If num is infinite and positive it should log out'To infinity'
-  if num  is finite and negative it should log out' To Negative infinity'.
-  if num is zero it should log out 'Staying home'
+   if  num (either positive or negative) it should be log out
+  'And Beyond'.  
+   if  num (is infinite and positive it should log out)'To infinity'
+   if  num ( is finite and negative it should log out) 'To Negative infinity'.
+   if num (is zero it should log out) 'Staying home'
 }
  {
    
