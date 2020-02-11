36c76bd9b8e57857a95492274bdd9f39bf3f2f68
Penelope Owens <owenspennie9@live.com>
2020-02-10 20:27:15 -0600

    JM.js

--- JM.js
+++ JM.js
@@ -1,25 +1,30 @@
 function getyearOfbirth(age) { 
     return 2020 - age;
 }
+
 function createGreeting(name, age){
     if ( name === undefined || age === undefined){
         throw new Error ('Arguements not valid.');
     }
+
 if (age < 0 ) {
     thow new Error('Age cannot be negative.');
 }
+
 if ( typeof age !== 'number'){
-    throw new TypeError ('Age must be a number');
+    throw new TypeError ( 'Age must be a number' );
 }
+
 const yob = getyearOfbirth(age);
 return `
 Hi, myy name is ${name} and I'm ${age} years old.
 I was born in ${yob}.
 `;
 }
+
 try {
     const greeting1 = createGreeting ('Penelope',45)
     console.log(greeting1);
 } catch(e){
-    console.error(e.message);
+console.error(e.message);
 }
